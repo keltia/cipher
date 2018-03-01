837a29bf22ee1392cba16b331c7a9890910a2a9a
Ollivier Robert <roberto@keltia.net>
2018-03-01 17:01:11 +0100

    Add Playfair.

--- cmd/old-crypto/main.go
+++ cmd/old-crypto/main.go
@@ -2,32 +2,67 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"github.com/keltia/cipher/caesar"
-	"github.com/keltia/cipher/null"
+	"github.com/keltia/cipher/playfair"
+	"github.com/keltia/cipher"
 )
+
+var (
+	fDebug bool
+)
+
+func init() {
+	flag.BoolVar(&fDebug, "D", false, "debug mode.")
+}
 
 func main() {
 	var plain = []byte("ABCDE")
 	var cipher = make([]byte, len(plain))
 
-	c, _ := null.NewCipher()
-	c.Encrypt(cipher, plain)
-	if !bytes.Equal(cipher, plain) {
-		fmt.Printf("plain: %s cipher: %s\n", string(plain), string(cipher))
-	}
+	flag.Parse()
 
+	fmt.Println("test starting.")
 	cipher = make([]byte, len(plain))
 
 	var mycipher = []byte("DEFGH")
 
-	c, _ = caesar.NewCipher(3)
+	fmt.Printf("pt=%s\n", string(plain))
+
+	c, _ := caesar.NewCipher(3)
 	c.Encrypt(cipher, plain)
 	if !bytes.Equal(mycipher, cipher) {
 		fmt.Printf("cipher: %s real: %s\n", string(mycipher), string(cipher))
 	}
+	fmt.Printf("ct=%s\n", string(cipher))
 
 	myplain := make([]byte, len(plain))
+
+	c.Decrypt(myplain, cipher)
+	if !bytes.Equal(myplain, plain) {
+		fmt.Printf("plain: %s real: %s\n", string(myplain), string(plain))
+	}
+
+	fmt.Println("-----------------")
+
+	plain = []byte("HIDETHEGOLDINTHETREESTUMP")
+	plain = crypto.Expand(plain)
+
+	mycipher = []byte("BMODZBXDNABEKUDMUIXMMOUVIF")
+	cipher = make([]byte, len(plain))
+
+	fmt.Printf("pt=%s\n", string(plain))
+
+	c, _ = playfair.NewCipher("PLAYFAIREXAMPLE")
+
+	c.Encrypt(cipher, plain)
+	if !bytes.Equal(mycipher, cipher) {
+		fmt.Printf("cipher: %s real: %s\n", string(mycipher), string(cipher))
+	}
+	fmt.Printf("ct=%s\n", string(cipher))
+
+	myplain = make([]byte, len(plain))
 
 	c.Decrypt(myplain, cipher)
 	if !bytes.Equal(myplain, plain) {
