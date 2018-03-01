59945403a9115c05f639627fa054715d50132d78
Ollivier Robert <roberto@keltia.net>
2018-03-01 17:01:36 +0100

    Add Expand() for splitting double letters.

--- crypto.go
+++ crypto.go
@@ -27,6 +27,28 @@ func Condense(str string) string {
 	return condensed
 }
 
+func Expand(src []byte) []byte {
+	var i int
+	var dst []byte
+
+	dst = append(dst, src[0])
+	j := 0
+	for i = 1; i <= len(src) - 1; {
+		if src[i] == dst[j] {
+			dst = append(dst, byte('X'))
+			j += 1
+			message("i=%d j=%d dst=%s", i, j, dst)
+		} else {
+			dst = append(dst, src[i])
+			i += 1
+			j += 1
+			message("i=%d j=%d dst=%s", i, j, dst)
+		}
+	}
+	message("---")
+	return dst
+}
+
 // verbose displays only if fVerbose is set
 func message(str string, a ...interface{}) {
 	log.Printf(str, a...)
