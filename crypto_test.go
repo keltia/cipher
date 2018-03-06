0a4d1f06219ed7012dcf5a7fe3d14a3411157972
Ollivier Robert <roberto@keltia.net>
2018-03-06 23:26:57 +0100

    Add benchmarks for Condense/Expand & fix test data for latter.
    XXX Ruby test data for Expand() is wrong.
        Go implement w/ insert() is correct.

--- crypto_test.go
+++ crypto_test.go
@@ -20,10 +20,43 @@ func TestCondense(t *testing.T) {
 	}
 }
 
+var bar string
+
+func BenchmarkCondense(b *testing.B) {
+	var r string
+
+	for n := 0; n < b.N; n++ {
+		for _, td := range testCondensedData {
+			r = Condense(td.a)
+		}
+	}
+	bar = r
+}
+
+func BenchmarkCondense1(b *testing.B) {
+	var r string
+
+	for n := 0; n < b.N; n++ {
+		for _, td := range testCondensedData {
+			r = Condense1(td.a)
+		}
+	}
+	bar = r
+}
+
 var testExpandData = []struct{ a, b string }{
 	{"AAA", "AXAXA"},
 	{"AAAA", "AXAXAXA"},
 	{"AAABRAACADAABRA", "AXAXABRAXACADAXABRA"},
+	{"ARABESQUE", "ARABESQUE"},
+	{"LANNONCE", "LANXNONCE"},
+	{"PJRJJJJJJS", "PJRJXJXJXJXJXJS"},
+}
+
+var testExpandInsertData = []struct{ a, b string }{
+	{"AAA", "AXAXA"},
+	{"AAAA", "AXAXAXA"},
+	{"AAABRAACADAABRA", "AXAXABRAACADAXABRA"},
 	{"ARABESQUE", "ARABESQUE"},
 	{"LANNONCE", "LANXNONCE"},
 	{"PJRJJJJJJS", "PJRJJXJXJXJXJS"},
@@ -33,4 +66,40 @@ func TestExpand(t *testing.T) {
 	for _, td := range testExpandData {
 		assert.EqualValues(t, []byte(td.b), Expand([]byte(td.a)))
 	}
+}
+
+func TestExpandInsert(t *testing.T) {
+	for _, td := range testExpandInsertData {
+		assert.EqualValues(t, []byte(td.b), ExpandInsert([]byte(td.a)))
+	}
+}
+
+func TestInsert(t *testing.T) {
+	a := []byte{0, 1, 2, 3}
+	b := []byte{0, 1, 42, 2, 3}
+	assert.EqualValues(t, b, insert(a, 42, 2))
+}
+
+var foo []byte
+
+func BenchmarkExpand(b *testing.B) {
+	var r []byte
+
+	for n := 0; n < b.N; n++ {
+		for _, td := range testExpandData {
+			r = Expand([]byte(td.a))
+		}
+	}
+	foo = r
+}
+
+func BenchmarkExpandInsert(b *testing.B) {
+	var r []byte
+
+	for n := 0; n < b.N; n++ {
+		for _, td := range testExpandData {
+			r = ExpandInsert([]byte(td.a))
+		}
+	}
+	foo = r
 }
