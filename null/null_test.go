4a554738bdc641120cc34d0e83968b8f610cd93e
Ollivier Robert <roberto@keltia.net>
2018-03-07 19:58:28 +0100

    Split tests and add benchamrks.

--- null/null_test.go
+++ null/null_test.go
@@ -1,6 +1,7 @@
 package null
 
 import (
+	"crypto/cipher"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -15,17 +16,68 @@ var encryptCaesarTests = []NullTest{
 }
 
 func TestNewCipher(t *testing.T) {
-	for _, pair := range encryptCaesarTests {
-		c, _ := NewCipher()
+	c, _ := NewCipher()
+	assert.NotNil(t, c)
+	assert.EqualValues(t, 1, c.BlockSize())
+}
 
-		assert.EqualValues(t, 1, c.BlockSize())
+func TestNullCipher_Encrypt(t *testing.T) {
+	c, _ := NewCipher()
+	assert.NotNil(t, c)
+	assert.EqualValues(t, 1, c.BlockSize())
+
+	for _, pair := range encryptCaesarTests {
 		plain := []byte(pair.pt)
 		cipher := make([]byte, len(plain))
 		c.Encrypt(cipher, plain)
 		assert.Equal(t, []byte(pair.ct), cipher)
+	}
+}
 
+func TestNullCipher_Decrypt(t *testing.T) {
+	c, _ := NewCipher()
+	assert.NotNil(t, c)
+	assert.EqualValues(t, 1, c.BlockSize())
+
+	for _, pair := range encryptCaesarTests {
+		plain := []byte(pair.pt)
+		cipher := []byte(pair.ct)
 		nplain := make([]byte, len(plain))
 		c.Decrypt(nplain, cipher)
 		assert.Equal(t, []byte(pair.pt), nplain)
+	}
+}
+
+var gc cipher.Block
+
+func BenchmarkNewCipher(b *testing.B) {
+	var c cipher.Block
+
+	for n := 0; n < b.N; n++ {
+		c, _ = NewCipher()
+	}
+	gc = c
+}
+
+func BenchmarkNullCipher_Encrypt(b *testing.B) {
+	c, _ := NewCipher()
+
+	plain := []byte("ABCDE")
+	cipher := make([]byte, len(plain))
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		c.Encrypt(cipher, plain)
+	}
+}
+
+func BenchmarkNullCipher_Decrypt(b *testing.B) {
+	c, _ := NewCipher()
+	cipher := []byte("ABCDE")
+	nplain := make([]byte, len(cipher))
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		c.Decrypt(nplain, cipher)
 	}
 }
