c3e637991d808d5dab2172cf5638db6e8bc3767e
Ollivier Robert <roberto@keltia.net>
2018-03-07 15:40:54 +0100

    Add test for Encrypt() and benchmark for both.

--- playfair/cipher_test.go
+++ playfair/cipher_test.go
@@ -21,7 +21,15 @@ func TestPlayfairCipher_BlockSize(t *testing.T) {
 }
 
 func TestPlayfairCipher_Encrypt(t *testing.T) {
+	c, _ := NewCipher("PLAYFAIREXAMPLE")
 
+	ct := []byte("BMODZBXDNABEKUDMUIXMMOUVIF")
+	pt := []byte("HIDETHEGOLDINTHETREXESTUMP")
+
+	dst := make([]byte, len(ct))
+
+	c.Encrypt(dst, pt)
+	assert.EqualValues(t, ct, dst)
 }
 
 func TestPlayfairCipher_Decrypt(t *testing.T) {
@@ -30,8 +38,32 @@ func TestPlayfairCipher_Decrypt(t *testing.T) {
 	ct := []byte("BMODZBXDNABEKUDMUIXMMOUVIF")
 	pt := []byte("HIDETHEGOLDINTHETREXESTUMP")
 
-	var dst []byte
+	dst := make([]byte, len(ct))
 
 	c.Decrypt(dst, ct)
 	assert.EqualValues(t, pt, dst)
+}
+
+func BenchmarkCipher_Encrypt(b *testing.B) {
+	c, _ := NewCipher("PLAYFAIREXAMPLE")
+
+	pt := []byte("HIDETHEGOLDINTHETREXESTUMP")
+
+	dst := make([]byte, len(pt))
+
+	for n := 0; n < b.N; n++ {
+		c.Encrypt(dst, pt)
+	}
+}
+
+func BenchmarkCipher_Decrypt(b *testing.B) {
+	c, _ := NewCipher("PLAYFAIREXAMPLE")
+
+	ct := []byte("BMODZBXDNABEKUDMUIXMMOUVIF")
+
+	dst := make([]byte, len(ct))
+
+	for n := 0; n < b.N; n++ {
+		c.Decrypt(dst, ct)
+	}
 }
