1074b49682c1ca24ed0f6e4f886852495bee66fd
Ollivier Robert <roberto@keltia.net>
2018-03-08 23:23:31 +0100

    Chaocipher implementation pass all tests.

--- chaocipher/cipher.go
+++ chaocipher/cipher.go
@@ -9,7 +9,7 @@ import (
 const (
 	alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	zenith   = 0
-	nadir    = 13
+	nadir    = 13 // 26/2 (+1 if one-based)
 )
 
 type chaocipher struct {
@@ -33,67 +33,69 @@ func (c *chaocipher) BlockSize() int {
 	return 1
 }
 
-/*
-   # Permute the two alphabets, first ciphertext then plaintext
-   # We use the current plain & ciphertext characters (akin to autoclave)
-   #
-   # Zenith is 0, Nadir is 13 (n/2 + 1 if 1-based)
-   # Steps for left:
-   # 1. shift from idx to Zenith
-   # 2. take Zenith+1 out
-   # 3. shift left one position and insert back the letter from step2
-   #
-   # Steps for right
-   # 1. shift everything from plain to Zenith
-   # 2. shift one more entire string
-   # 3. extract Zenith+2
-   # 4. shift from Zenith+3 to Nadir left
-   # 5. insert  letter from step 3 in place
-   #
-   def advance(idx)
-     if idx != 0 then
-       cw = @cipher[idx..-1] + @cipher[ZENITH..(idx - 1)]
-       pw = @plain[idx..-1] + @plain[ZENITH..(idx - 1)]
-     else
-       cw = @cipher
-       pw = @plain
-     end
-     @cipher = cw[ZENITH].chr + cw[(ZENITH + 2)..NADIR] + \
-             cw[ZENITH + 1].chr + cw[(NADIR + 1)..-1]
-     raise DataError, 'cw length bad' if cw.length != BASE.length
+func lshift(a []byte) {
+	f := a[0]
+	copy(a, a[1:])
+	a[len(a)-1] = f
+}
 
-     pw = pw[(ZENITH + 1)..-1] + pw[ZENITH].chr
-     @plain = pw[ZENITH..(ZENITH + 1)] + pw[(ZENITH + 3)..NADIR] + \
-            pw[ZENITH + 2].chr + pw[(NADIR + 1)..-1]
-     raise DataError, 'pw length bad' if pw.length != BASE.length
-   end # -- advance
+func rshift(a []byte) {
+	f := a[len(a)-1]
+	copy(a[1:], a[0:len(a)-1])
+	a[0] = f
+}
+
+func lshiftN(a []byte, n int) {
+	f := dup(a[0:n])
+	copy(a, a[n:])
+	copy(a[len(a)-n:], f)
+}
+
+func rshiftN(a []byte, n int) {
+	f := dup(a[len(a)-n:])
+	copy(a[n:], a[0:len(a)-n])
+	copy(a, f)
+}
+
+func dup(a []byte) []byte {
+	b := make([]byte, len(a))
+	copy(b, a)
+	return b
+}
+
+/*
+Permute the two alphabets, first ciphertext then plaintext
+We use the current plain & ciphertext characters (akin to autoclave)
+
+Zenith is 0, Nadir is 13 (n/2 + 1 if 1-based)
+
+Steps for left:
+ 1. shift from idx to Zenith
+ 2. take Zenith+1 out
+ 3. shift left one position and insert back the letter from step2
+
+Steps for right
+ 1. shift everything from plain to Zenith
+ 2. shift one more entire string
+ 3. extract Zenith+2
+ 4. shift from Zenith+3 to Nadir left
+ 5. insert  letter from step 3 in place
+
+XXX due to the way Go manages the array memory & copy() works with slices, there are
+index & length differences with Ruby
 */
 func (c *chaocipher) advance(idx int) {
-	var pw, cw []byte
+	// First we shift the left alphabet (cw)
+	lshiftN(c.cw, idx)
+	l := c.cw[zenith+1]
+	copy(c.cw[zenith+1:nadir], c.cw[zenith+2:nadir+1])
+	c.cw[nadir] = l
 
-	if idx != 0 {
-		pw = append(c.pw[idx:], c.pw[zenith:idx]...)
-		cw = append(c.cw[idx:], c.cw[zenith:idx]...)
-	} else {
-		pw = c.pw
-		cw = c.cw
-	}
-
-	// shift ciphertext wheel
-	tcw := []byte{cw[zenith]}
-	tcw = append(tcw, cw[zenith+2:nadir]...)
-	tcw = append(tcw, cw[zenith+1])
-	tcw = append(tcw, cw[nadir+1:]...)
-	c.cw = tcw
-
-	// shift plaintext wheel
-	tpw := cw[zenith+1:]
-	tpw = append(tpw, pw[zenith])
-	tpw = append(tpw, pw[zenith:zenith+1]...)
-	tpw = append(tpw, pw[zenith+3:nadir]...)
-	tpw = append(tpw, pw[zenith+2])
-	tpw = append(tpw, pw[nadir+1:]...)
-	c.pw = tpw
+	// Then we shift the right alphabet (pw)
+	lshiftN(c.pw, idx+1)
+	l = c.pw[zenith+2]
+	copy(c.pw[zenith+2:nadir], c.pw[zenith+3:nadir+1])
+	c.pw[nadir] = l
 }
 
 func (c *chaocipher) encodeBoth(r1, r2 []byte, ch byte) byte {
@@ -112,9 +114,13 @@ func (c *chaocipher) decode(ch byte) byte {
 }
 
 func (c *chaocipher) Encrypt(dst, src []byte) {
-
+	for i, ch := range src {
+		dst[i] = c.encode(ch)
+	}
 }
 
 func (c *chaocipher) Decrypt(dst, src []byte) {
-
+	for i, ch := range src {
+		dst[i] = c.decode(ch)
+	}
 }
