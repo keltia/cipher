a1fc9921bf1bfd95c8d8cafabcc2cdac3596a488
Ollivier Robert <roberto@keltia.net>
2018-03-11 18:45:04 +0100

    Add transposition + tests/benchmarks.

---
+++ transposition/cipher.go
@@ -0,0 +1,90 @@
+package transposition
+
+import (
+	"bytes"
+	"crypto/cipher"
+	"github.com/keltia/cipher"
+	"log"
+)
+
+type transp struct {
+	key  string
+	tkey []byte
+}
+
+func NewCipher(key string) (cipher.Block, error) {
+	c := &transp{
+		key:  key,
+		tkey: crypto.ToNumeric(key),
+	}
+	return c, nil
+}
+
+func (c *transp) BlockSize() int {
+	return len(c.tkey)
+}
+
+func (c *transp) Encrypt(dst, src []byte) {
+	klen := len(c.tkey)
+	table := make([]bytes.Buffer, klen)
+
+	// Fill-in the table
+	for i, ch := range src {
+		table[i%klen].WriteByte(ch)
+	}
+
+	var res bytes.Buffer
+
+	// Extract each column in order
+	for i := 0; i < klen; i++ {
+		j := bytes.IndexByte(c.tkey, byte(i))
+		res.Write(table[j].Bytes())
+	}
+	copy(dst, res.Bytes())
+}
+
+func (c *transp) Decrypt(dst, src []byte) {
+	klen := len(c.tkey)
+	table := make([]bytes.Buffer, klen)
+	scol := len(src)/klen + 1
+
+	col := make([]byte, scol)
+	pcol := make([]byte, scol-1)
+	var pt = bytes.NewBuffer(src)
+
+	// Find how many columns are not filled in (irregular table)
+	pad := len(src) % klen // col 0..pad-1 are complete
+
+	j := 0
+	for i := 0; i < len(src); {
+
+		howMany := scol
+		ind := bytes.IndexByte(c.tkey, byte(j))
+
+		// if on a non-complete column
+		if ind >= pad {
+			howMany--
+
+			pt.Read(pcol)
+			table[ind].Write(pcol)
+		} else {
+			// Complete column
+			pt.Read(col)
+			table[ind].Write(col)
+		}
+
+		i += howMany
+		j++
+	}
+
+	// Now get all text
+	for i := range src {
+		pt, _ := table[i%klen].ReadByte()
+		dst[i] = pt
+	}
+}
+
+// verbose displays only if fVerbose is set
+func message(str string, a ...interface{}) {
+	log.Printf(str, a...)
+}
---
+++ transposition/cipher_test.go
@@ -0,0 +1,111 @@
+package transposition
+
+import (
+	"bytes"
+	"crypto/cipher"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewCipher(t *testing.T) {
+	c, err := NewCipher("ABCDE")
+
+	assert.NotNil(t, c)
+	assert.NoError(t, err)
+
+	cc := c.(*transp)
+
+	assert.Equal(t, "ABCDE", cc.key)
+	assert.EqualValues(t, []byte{0, 1, 2, 3, 4}, cc.tkey)
+}
+
+func TestTransp_BlockSize(t *testing.T) {
+	c, err := NewCipher("ABCDE")
+
+	assert.NotNil(t, c)
+	assert.NoError(t, err)
+	assert.Equal(t, 5, c.BlockSize())
+}
+
+func TestTransp_Encrypt(t *testing.T) {
+	pt := bytes.NewBufferString("ATTACKATDAWNATPOINT42X23XSENDMOREMUNITIONSBYNIGHTX123")
+
+	cts := []struct{ k, c string }{
+		{"ARABESQUE", "AATNIITN2MIHAAXOOTCT2RNXDNENNAOXMB2TW4DTGKP3ES1TISUY3"},
+		{"SUBWAY", "CWI2DUNG3TDP2EEIN1AAATXOIBTTTT4SRTYXAAOXNMOI2KNN3MNSH"},
+		{"PORTABLE", "CA2DIN3KTXMTITO3ROHAP2OIGTANSMSXADIXENTTWTEUB1AN4NNY2"},
+	}
+
+	for _, cti := range cts {
+		ct := bytes.NewBufferString(cti.c)
+		key := cti.k
+
+		c, err := NewCipher(key)
+		assert.NotNil(t, c)
+		assert.NoError(t, err)
+
+		dst := make([]byte, pt.Len())
+		c.Encrypt(dst, pt.Bytes())
+
+		assert.EqualValues(t, ct.Bytes(), dst)
+	}
+}
+
+func TestTransp_Decrypt(t *testing.T) {
+	pt := bytes.NewBufferString("ATTACKATDAWNATPOINT42X23XSENDMOREMUNITIONSBYNIGHTX123")
+
+	cts := []struct{ k, c string }{
+		{"ARABESQUE", "AATNIITN2MIHAAXOOTCT2RNXDNENNAOXMB2TW4DTGKP3ES1TISUY3"},
+		{"SUBWAY", "CWI2DUNG3TDP2EEIN1AAATXOIBTTTT4SRTYXAAOXNMOI2KNN3MNSH"},
+		{"PORTABLE", "CA2DIN3KTXMTITO3ROHAP2OIGTANSMSXADIXENTTWTEUB1AN4NNY2"},
+	}
+
+	for _, cti := range cts {
+		ct := bytes.NewBufferString(cti.c)
+		key := cti.k
+
+		c, err := NewCipher(key)
+		assert.NotNil(t, c)
+		assert.NoError(t, err)
+
+		dst := make([]byte, pt.Len())
+		c.Decrypt(dst, ct.Bytes())
+
+		assert.EqualValues(t, pt.Bytes(), dst)
+	}
+
+}
+
+// ----- benchmark
+
+var gc cipher.Block
+
+func BenchmarkNewCipher(b *testing.B) {
+	var c cipher.Block
+
+	for n := 0; n < b.N; n++ {
+		c, _ = NewCipher("ABCDE")
+	}
+	gc = c
+}
+
+func BenchmarkTransp_Encrypt(b *testing.B) {
+	pt := bytes.NewBufferString("ATTACKATDAWNATPOINT42X23XSENDMOREMUNITIONSBYNIGHTX123")
+	key := "ARABESQUE"
+
+	c, _ := NewCipher(key)
+	dst := make([]byte, pt.Len())
+	for n := 0; n < b.N; n++ {
+		c.Encrypt(dst, pt.Bytes())
+	}
+}
+
+func BenchmarkTransp_Decrypt(b *testing.B) {
+	ct := bytes.NewBufferString("AATNIITN2MIHAAXOOTCT2RNXDNENNAOXMB2TW4DTGKP3ES1TISUY3")
+	key := "ARABESQUE"
+	c, _ := NewCipher(key)
+	dst := make([]byte, ct.Len())
+	for n := 0; n < b.N; n++ {
+		c.Encrypt(dst, ct.Bytes())
+	}
+}
